96049e77df6e7bbe4ca93ec2f833afe2be20ed9d
OttoAnsori <fathan.m.ansori.stu@pnc.ac.id>
2025-10-28 17:36:34 +0700

    Repository

---
+++ src/main/java/com/praktikum/repository/StudentRepository.java
@@ -0,0 +1,31 @@
+package com.praktikum.repository;
+
+import com.praktikum.model.Course;
+import com.praktikum.model.Student;
+import java.util.List;
+
+/**
+ * Interface untuk akses data mahasiswa
+ * Interface ini akan di-stub atau di-mock dalam unit testing
+ */
+public interface StudentRepository {
+    /**
+     * Mencari mahasiswa berdasarkan student ID
+     * @param studentId ID mahasiswa
+     * @return Student object atau null jika tidak ditemukan
+     */
+    Student findById(String studentId);
+
+    /**
+     * Update data mahasiswa
+     * @param student Student object yang akan diupdate
+     */
+    void update(Student student);
+
+    /**
+     * Mendapatkan daftar mata kuliah yang sudah diselesaikan mahasiswa
+     * @param studentId ID mahasiswa
+     * @return List of Course yang sudah diselesaikan
+     */
+    List<Course> getCompletedCourses(String studentId);
+}
